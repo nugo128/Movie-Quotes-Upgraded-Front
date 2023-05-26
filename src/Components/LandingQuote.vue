9b0c9dd31e0168920b69ccc6909c5427c4c5b29e
nugo128 <nugo.rostiashvili@redberry.ge>
2023-05-26 10:32:43 +0400

    style: parallax scrolling

---
+++ src/Components/LandingQuote.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="relative fit h-screen">
+    <div :class="handleScroll" class="h-[100vh] pt-[50%] pl-[10%] lg:pt-[200px] lg:pl-44">
+      <div>
+        <h2>{{ quote }}</h2>
+        <h3>{{ scroll }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  scroll: { type: Boolean, required: true },
+  image: { type: String, required: true },
+  quote: { type: String, required: true },
+  movie: { type: String, required: true }
+})
+const handleScroll = computed(() => {
+  if (props.scroll) {
+    return `paralax ${props.image}`
+  }
+  return props.image
+})
+</script>
+
+<style>
+.fit {
+  background-size: 100% 100%;
+}
+.paralax {
+  background-attachment: fixed;
+}
+</style>
--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-3/4 bg-black flex flex-col gap-64 pb-[200px]">
+  <div class="bg-black flex flex-col gap-64 pb-[180px]">
     <div class="flex justify-between px-8 py-4">
       <h3 class="text-[#DDCCAA]">MOVIE QUOTES</h3>
       <div class="flex gap-6">
@@ -15,7 +15,49 @@
     <div class="flex flex-col items-center">
       <h2 class="text-[#DDCCAA] text-[60px]">Find any quote in</h2>
       <h2 class="text-[#DDCCAA] text-[60px]">millions of movie lines</h2>
-      <button class="bg-[#E31221] text-white p-3 rounded">Get started</button>
+      <button class="bg-[#E31221] text-white p-3 rounded mt-5">Get started</button>
     </div>
   </div>
+  <landing-quote
+    image="bg-[url('@/assets/images/interstelar.png')]"
+    quote="“You have to leave somethig 
+behind to go forward”"
+    movie="Interstellar,2014"
+    :scroll="scroll"
+  >
+  </landing-quote>
+  <landing-quote
+    image="bg-[url('@/assets/images/tenabaums.png')]"
+    quote="I think we’re just gonna have
+  to be secretly in love with earch other
+  and leave it that"
+    movie="The Royal Tenenbaums,2001 "
+    :scroll="scroll"
+  >
+  </landing-quote>
+  <landing-quote
+    image="bg-[url('@/assets/images/LOTR.png')]"
+    quote="I think we’re just gonna have
+  to be secretly in love with earch other
+  and leave it that"
+    movie="The Royal Tenenbaums,2001 "
+    :scroll="scroll"
+  >
+  </landing-quote>
 </template>
+
+<script setup>
+import LandingQuote from '../Components/LandingQuote.vue'
+import { ref } from 'vue'
+let scroll = ref(false)
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight * 0.75 <= window.scrollY) {
+    scroll.value = true
+  } else {
+    scroll.value = false
+  }
+})
+</script>
+
+<style></style>
