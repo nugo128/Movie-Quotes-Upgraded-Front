d56a61b72dca86f6b8de9d508f55061259d50907
nugo128 <nugo.rostiashvili@redberry.ge>
2023-06-16 18:56:49 +0400

    feat: switch between newsfeed and user profile

---
+++ src/pages/UserProfile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <profile-header></profile-header>
+    <div class="pt-8">
+      <user-navbar></user-navbar>
+      <div class="w-[900px] ml-[503px] mt-28">
+        <h2 class="text-white">profile</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import profileHeader from '../Components/profileHeader.vue'
+import UserNavbar from '../Components/UserNavbar.vue'
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../pages/LandingPage.vue'
 import NewsFeed from '../pages/NewsFeed.vue'
+import UserProfile from '../pages/UserProfile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,11 @@ const router = createRouter({
       path: '/newsfeed',
       name: 'news_feed',
       component: NewsFeed
+    },
+    {
+      path: '/user-profile',
+      name: 'user_profile',
+      component: UserProfile
     }
   ]
 })
