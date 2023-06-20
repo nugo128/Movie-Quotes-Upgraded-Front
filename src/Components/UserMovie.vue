90ae6a1575ee89223a86b598d5f12b194c588df5
nugo128 <nugo.rostiashvili@redberry.ge>
2023-06-20 19:54:36 +0400

    style: movies page basic markup

---
+++ src/Components/UserMovie.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <img src="../assets/images/interstelar.png" alt="" class="w-440 h-370 rounded-xl" />
+    <h2 class="text-white text-3xl">MOVIE TITLE (<span>2023</span>)</h2>
+    <div class="flex gap-3">
+      <h2 class="text-white text-2xl">10</h2>
+      <img src="../assets/images/quoteIcon.svg" alt="" class="w-8 h-7" />
+    </div>
+  </div>
+</template>
--- src/pages/MyMovies.vue
+++ src/pages/MyMovies.vue
@@ -3,7 +3,23 @@
     <profile-header></profile-header>
     <div>
       <user-navbar></user-navbar>
-      <div class="text-white p-96">movies</div>
+      <div class="pl-440 pt-28 pr-16">
+        <div class="text-white flex items-center justify-between mb-16">
+          <h2 class="text-lg">total num of movies <span>30</span></h2>
+          <div class="flex items-center gap-4">
+            <search-bar></search-bar>
+            <button class="py-2 px-4 bg-[#E31221] rounded-lg flex gap-2 items-center">
+              <img src="../assets/images/plus.svg" alt="" />Add movie
+            </button>
+          </div>
+        </div>
+        <div class="h-max flex flex-wrap gap-10">
+          <user-movie></user-movie>
+          <user-movie></user-movie>
+          <user-movie></user-movie>
+          <user-movie></user-movie>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,4 +27,6 @@
 <script setup>
 import UserNavbar from '../Components/UserNavbar.vue'
 import profileHeader from '../Components/profileHeader.vue'
+import SearchBar from '../Components/SearchBar.vue'
+import UserMovie from '../Components/UserMovie.vue'
 </script>
